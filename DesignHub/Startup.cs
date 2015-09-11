a953a3b82b92c3fecedbc451743efcd64aa9d873
rafalboguski <boguskirafal@gmail.com>
2015-09-11 18:12:04 +0200

    Proper connection string

---
+++ DesignHub/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using DesignHub;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace DesignHub
+{
+    public class Startup              
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
