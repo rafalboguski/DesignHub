5eb659809a0c7c22907f1345a97308c4207653d5
rafalboguski <boguskirafal@gmail.com>
2015-09-12 18:51:09 +0200

    Token authentication, login and register

---
+++ DesignHub/Controllers/ProjectController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+
+namespace DesignHub.Controllers
+{
+    [RoutePrefix("api/Projects")]
+    public class ProjectController : ApiController
+    {
+
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            var list = Project.CreateProjects();
+            return Json(list);
+        }
+
+    }
+
+    public class Project
+    {
+        public int ProjectID { get; set; }
+        public string Name { get; set; }
+        public bool Finished { get; set; }
+
+        public Project(int projectId, string name, bool finished)
+        {
+            ProjectID = projectId;
+            Name = name;
+            Finished = finished;
+        }
+
+
+        public static List<Project> CreateProjects()
+        {
+            var list = new List<Project>
+            {
+              new Project(10001, "Ziemniak", false),
+              new Project(10002, "Kartofel", true)
+            };
+            return list;
+        }
+    }
+}
