c8f3b72bfce20248cd1da196bcf8515feec92a8e
rafalboguski <boguskirafal@gmail.com>
2015-09-13 19:28:42 +0200

    Sidebar menu prototype

--- Frontend/app/controllers/indexController.js
+++ Frontend/app/controllers/indexController.js
@@ -8,4 +8,27 @@ app.controller('indexController', ['$scope', '$location', 'authService', functio
 
     $scope.authentication = authService.authentication;
 
+
+
+    // Horizontal menu -------------------------------
+    $scope.noneStyle = false;
+    $scope.bodyCon = false;
+
+    //Toggle the styles
+    $scope.toggleStyle = function () {
+        //If they are true, they will become false 
+        //and false will become true
+        $scope.bodyCon = !$scope.bodyCon;
+        $scope.noneStyle = !$scope.noneStyle;
+    }
+    //add class to search box
+    $scope.openSearch = false;
+    $scope.searchToggle = function () {
+
+        $scope.openSearch = !$scope.openSearch;
+
+    }
+   // -----------------------------------------------
+
+
 }]);
