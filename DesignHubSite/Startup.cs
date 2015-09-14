eb02473aa94cdb479edf0080789ac13e31a74a60
rafalboguski <boguskirafal@gmail.com>
2015-09-14 23:38:53 +0200

    initial, new project

---
+++ DesignHubSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DesignHubSite.Startup))]
+namespace DesignHubSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
