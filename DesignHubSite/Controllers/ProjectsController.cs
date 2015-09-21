fec3cbfab1ab5cfbff7ea168dc490a2cb3c4b753
rafalboguski <boguskirafal@gmail.com>
2015-09-21 21:37:08 +0200

    Add new project test

--- DesignHubSite/Controllers/ProjectsController.cs
+++ DesignHubSite/Controllers/ProjectsController.cs
@@ -17,34 +17,35 @@ namespace DesignHubSite.Controllers
     {
         private readonly ApplicationDbContext _db = new ApplicationDbContext();
 
+        
         [Route("")]
         public IHttpActionResult GetProjects()
         {
             return Json(_db.Projects);
         }
 
-        
-        [ResponseType(typeof(Project))]
-        public IHttpActionResult GetProject(int id)
-        {
-            var project = _db.Projects.Find(id);
-            if (project == null)
-            {
-                return NotFound();
-            }
 
-            return Json("sdfsdf");
-        }
+        //        [ResponseType(typeof(Project))]
+        //        public IHttpActionResult GetProject(int id)
+        //        {
+        //            var project = _db.Projects.Find(id);
+        //            if (project == null)
+        //            {
+        //                return NotFound();
+        //            }
+        //
+        //            return Json("sdfsdf");
+        //        }
 
+        [HttpGet]
         [Route("dodaj")]
-        [ResponseType(typeof(Project))]
-        public IHttpActionResult Dupa(int id)
+        public IHttpActionResult Dodaj()
         {
 
             var p = new Project
             {
-                Name = "sdfsdf",
-                Description = "sdf"
+                Name = "aaa",
+                Description = "aaa"
             };
 
             _db.Projects.Add(p);
@@ -53,84 +54,84 @@ namespace DesignHubSite.Controllers
             return Ok();
         }
 
-        // PUT: api/Projects/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutProject(int id, Project project)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != project.Id)
-            {
-                return BadRequest();
-            }
-
-            _db.Entry(project).State = EntityState.Modified;
-
-            try
-            {
-                _db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProjectExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Projects
-        [ResponseType(typeof(Project))]
-        public IHttpActionResult PostProject(Project project)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _db.Projects.Add(project);
-            _db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = project.Id }, project);
-        }
-
-        // DELETE: api/Projects/5
-        [ResponseType(typeof(Project))]
-        public IHttpActionResult DeleteProject(int id)
-        {
-            var project = _db.Projects.Find(id);
-            if (project == null)
-            {
-                return NotFound();
-            }
-
-            _db.Projects.Remove(project);
-            _db.SaveChanges();
-
-            return Ok(project);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool ProjectExists(int id)
-        {
-            return _db.Projects.Count(e => e.Id == id) > 0;
-        }
+        //        // PUT: api/Projects/5
+        //        [ResponseType(typeof(void))]
+        //        public IHttpActionResult PutProject(int id, Project project)
+        //        {
+        //            if (!ModelState.IsValid)
+        //            {
+        //                return BadRequest(ModelState);
+        //            }
+        //
+        //            if (id != project.Id)
+        //            {
+        //                return BadRequest();
+        //            }
+        //
+        //            _db.Entry(project).State = EntityState.Modified;
+        //
+        //            try
+        //            {
+        //                _db.SaveChanges();
+        //            }
+        //            catch (DbUpdateConcurrencyException)
+        //            {
+        //                if (!ProjectExists(id))
+        //                {
+        //                    return NotFound();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
+        //
+        //            return StatusCode(HttpStatusCode.NoContent);
+        //        }
+        //
+        //        // POST: api/Projects
+        //        [ResponseType(typeof(Project))]
+        //        public IHttpActionResult PostProject(Project project)
+        //        {
+        //            if (!ModelState.IsValid)
+        //            {
+        //                return BadRequest(ModelState);
+        //            }
+        //
+        //            _db.Projects.Add(project);
+        //            _db.SaveChanges();
+        //
+        //            return CreatedAtRoute("DefaultApi", new { id = project.Id }, project);
+        //        }
+        //
+        //        // DELETE: api/Projects/5
+        //        [ResponseType(typeof(Project))]
+        //        public IHttpActionResult DeleteProject(int id)
+        //        {
+        //            var project = _db.Projects.Find(id);
+        //            if (project == null)
+        //            {
+        //                return NotFound();
+        //            }
+        //
+        //            _db.Projects.Remove(project);
+        //            _db.SaveChanges();
+        //
+        //            return Ok(project);
+        //        }
+        //
+        //        protected override void Dispose(bool disposing)
+        //        {
+        //            if (disposing)
+        //            {
+        //                _db.Dispose();
+        //            }
+        //            base.Dispose(disposing);
+        //        }
+        //
+        //        private bool ProjectExists(int id)
+        //        {
+        //            return _db.Projects.Count(e => e.Id == id) > 0;
+        //        }
     }
 }
