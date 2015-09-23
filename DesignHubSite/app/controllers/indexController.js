45c35729dcbc1d9180d47d8eb93bf410c36a100f
rafalboguski <boguskirafal@gmail.com>
2015-09-23 15:35:55 +0200

    Frontend Sidemenu

--- DesignHubSite/app/controllers/indexController.js
+++ DesignHubSite/app/controllers/indexController.js
@@ -2,11 +2,17 @@
 app.controller('indexController', ['$scope', function ($scope) {
 
     $scope.user_logged = false;
+    $scope.current_page = "no set";
 
     $scope.init = function (isLogged) {
 
         if (isLogged === 'True')
             $scope.user_logged = true;
+
+    };
+    $scope.updateLocation = function (page) {
+
+        $scope.current_page = page;
 
     };
 
