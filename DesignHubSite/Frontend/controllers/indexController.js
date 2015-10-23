d2216e36e812723030d010b408314a273a0abeea
rafalboguski <BoguskiRafal@gmail.com>
2015-10-23 21:48:47 +0200

    New Projecs view grid

--- DesignHubSite/Frontend/controllers/indexController.js
+++ DesignHubSite/Frontend/controllers/indexController.js
@@ -2,7 +2,7 @@
 app.controller('indexController', ['$scope', function ($scope) {
 
     $scope.user_logged = false;
-    $scope.current_page = "no set";
+    $scope.current_page = "projects";
 
     $scope.init = function (isLogged) {
 
